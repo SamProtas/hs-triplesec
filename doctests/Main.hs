8d4b7702ca9051c68a24c09b38ea202242003aa1
Sam Protas <smprts@gmail.com>
2019-06-26 20:19:56 -0400

    Fix doctests for new stack behavior

--- doctests/Main.hs
+++ doctests/Main.hs
@@ -4,4 +4,4 @@ module Main where
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["-XOverloadedStrings", "-XScopedTypeVariables", "src/Crypto/TripleSec/Tutorial.hs"]
+main = doctest ["-XOverloadedStrings", "-XScopedTypeVariables", "-isrc", "src/Crypto/TripleSec/Tutorial.hs"]
